2734304f4877f624c167bc1bc0f15cbbfa509f92
zhangyuheng <zhangyuheng@lunadeer.cn>
2024-12-20 14:05:10 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("java")
+    id("maven-publish")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    withJavadocJar()
+    withSourcesJar()
+}
+
+group = "cn.lunadeer"
+version = "1.0-ALPHA"
+
+// utf-8
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/groups/public")
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = project.name
+            groupId = project.group.toString()
+            version = project.version.toString()
+            // Add only the necessary artifacts manually to avoid conflicts
+            artifact(tasks.jar.get())  // The main JAR file without classifier
+            artifact(tasks.named<Jar>("sourcesJar").get()) {
+                classifier = "sources"
+            }
+            artifact(tasks.named<Jar>("javadocJar").get()) {
+                classifier = "javadoc"
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://ssl.lunadeer.cn:14454/repository/maven-snapshots/")
+            credentials {
+                // from m2 settings.xml
+                username = project.findProperty("nexusUsername")?.toString()
+                password = project.findProperty("nexusPassword")?.toString()
+            }
+        }
+    }
+}
