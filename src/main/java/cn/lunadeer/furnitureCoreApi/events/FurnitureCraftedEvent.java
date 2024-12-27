28eb40a126f02f83d96d1762b593be3200f27592
zhangyuheng <zhangyuheng@lunadeer.cn>
2024-12-27 11:05:08 +0800

    made FurnitureItemStack as api

--- src/main/java/cn/lunadeer/furnitureCoreApi/events/FurnitureCraftedEvent.java
+++ src/main/java/cn/lunadeer/furnitureCoreApi/events/FurnitureCraftedEvent.java
@@ -1,4 +1,24 @@
 package cn.lunadeer.furnitureCoreApi.events;
 
-public class FurnitureCraftedEvent extends CancellableEvent{
+import cn.lunadeer.furnitureCoreApi.items.FurnitureItemStack;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.Recipe;
+import org.jetbrains.annotations.NotNull;
+
+public class FurnitureCraftedEvent extends CraftItemEvent {
+    private final FurnitureItemStack furnitureItemStack;
+
+    public FurnitureCraftedEvent(@NotNull Recipe recipe, @NotNull InventoryView what, @NotNull InventoryType.SlotType type, int slot, @NotNull ClickType click, @NotNull InventoryAction action, FurnitureItemStack furnitureItemStack) {
+        super(recipe, what, type, slot, click, action);
+        this.furnitureItemStack = furnitureItemStack;
+    }
+
+    public FurnitureItemStack getFurnitureResult() {
+        return furnitureItemStack;
+    }
+
 }
---
+++ src/main/java/cn/lunadeer/furnitureCoreApi/items/ScrewdriverItemStack.java
@@ -0,0 +1,78 @@
+package cn.lunadeer.furnitureCoreApi.items;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.List;
+
+public class ScrewdriverItemStack extends ItemStack {
+    private static final NamespacedKey pdcKey = new NamespacedKey("furniture_core", "tools/screwdriver");
+
+    /**
+     * Construct a Screwdriver item.
+     */
+    public ScrewdriverItemStack() {
+        super(Material.STICK);
+        setItemMeta();
+    }
+
+    /**
+     * Construct a Screwdriver item with specific amount.
+     *
+     * @param amount The amount of the item.
+     */
+    public ScrewdriverItemStack(int amount) {
+        super(Material.STICK, amount);
+        setItemMeta();
+    }
+
+    /**
+     * Construct a Screwdriver item from a stick item.
+     *
+     * @param item The stick item.
+     * @throws IllegalArgumentException If the item is not a stick or not a Screwdriver item.
+     */
+    public ScrewdriverItemStack(ItemStack item) throws IllegalArgumentException {
+        super(Material.STICK, item.getAmount());
+        if (item.getType() != Material.STICK) {
+            throw new IllegalArgumentException("Not a stick.");
+        }
+        if (item.getItemMeta() == null) {
+            throw new IllegalArgumentException("Item meta not found.");
+        }
+        if (!item.getItemMeta().getPersistentDataContainer().has(pdcKey)) {
+            throw new IllegalArgumentException("Not a Screwdriver item.");
+        }
+        setItemMeta();
+    }
+
+    private void setItemMeta() {
+        ItemMeta meta = getItemMeta();
+        meta.displayName(Component.text("Screwdriver"));
+        meta.getPersistentDataContainer().set(pdcKey, PersistentDataType.INTEGER, 1);
+        meta.setItemModel(pdcKey);
+        meta.lore(List.of(
+                Component.text("Left click to break furniture."),
+                Component.text("Right click to rotate furniture.")
+        ));
+        setItemMeta(meta);
+    }
+
+    /**
+     * Get the recipe of Screwdriver.
+     *
+     * @return The recipe of Screwdriver.
+     */
+    public static ShapelessRecipe getRecipe() {
+        ShapelessRecipe recipe = new ShapelessRecipe(pdcKey, new ScrewdriverItemStack());
+        recipe.addIngredient(Material.STICK);
+        recipe.addIngredient(Material.IRON_BARS);
+        return recipe;
+    }
+
+}
